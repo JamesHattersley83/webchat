def5eb9f330a64f98c32ba9c7cdaddd8f5616147
James Hattersley <jameshattersley83@gmail.com>
2019-09-14 22:03:40 +0100

    feat/create server and set up user route

--- api/main.js
+++ api/main.js
@@ -1,5 +1,20 @@
 require("dotenv").config();
 const logger = require("./logger");
+const express = require("express");
+const user = require("./routes/user");
+const app = express();
 
-logger.info("DEBUG");
-logger.error("An exception has occured");
+app.use("/user", user);
+
+// return 404 status when route does not match
+app.get("*", function(req, res) {
+  res.status(404).send("Bad Request...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  logger.info(`Server listening on port ${PORT}...`);
+});
+
+// logger.info("DEBUG");
+// logger.error("An exception has occured");

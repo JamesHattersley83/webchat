d934da46f6c269cd2020f051bd3a3984ff411c42
James Hattersley <jameshattersley83@gmail.com>
2019-09-26 22:34:06 +0100

    feat: mongoDB setup and connection

--- api/main.js
+++ api/main.js
@@ -1,19 +1,35 @@
-require("dotenv").config();
-const logger = require("./logger");
-const express = require("express");
-const user = require("./routes/user");
+require('dotenv').config();
+const logger = require('./logger');
+const express = require('express');
+const user = require('./routes/user');
+const mongoose = require('mongoose');
 const app = express();
 
-app.use("/user", user);
+const PORT = process.env.PORT || 5000;
 
-// return 404 status when route does not match
-app.get("*", function(req, res) {
-  res.status(404).send("Bad Request...");
+// connect to db
+const dbURL = process.env.MONGO_DB_URL;
+
+const server = new Promise((resolve, reject) => {
+  mongoose.connect(dbURL, { useNewUrlParser: true }, err => {
+    if (err) {
+      logger.error(`Error connecting to: ${dbURL}`);
+      return reject(err);
+    }
+    logger.info(`Connected to MongoDB database: ${dbURL}`);
+    return resolve();
+  });
+}).then(() => {
+  app.listen(PORT, () => {
+    logger.info(`Server listening on port ${PORT}...`);
+  });
 });
 
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => {
-  logger.info(`Server listening on port ${PORT}...`);
+app.use('/user', user);
+
+// return 404 status when route does not match
+app.get('*', (req, res) => {
+  res.status(404).send('Bad Request...');
 });
 
 module.exports = server;

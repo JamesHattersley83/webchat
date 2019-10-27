a8bd7c66b8d1f5240940ec8a93af19fc2db6fa74
James Hattersley <jameshattersley83@gmail.com>
2019-10-27 23:01:30 +0000

    feat: setup react app with webpack and babel

---
+++ app/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/client/index.html'
+    })
+  ]
+};

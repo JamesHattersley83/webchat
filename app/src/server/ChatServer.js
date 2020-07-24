efb02651e1d3af5890539e8556f4c0609b3a8436
James Hattersley <jameshattersley83@gmail.com>
2020-07-24 10:55:58 +0100

    modified handlers in server

--- app/src/server/ChatServer.js
+++ app/src/server/ChatServer.js
@@ -18,21 +18,23 @@ module.exports = class ChatServer {
         socket.emit(chatConstants.USERS, {
           users: [{ userid: userid, username: username }],
         });
-        // send message to all connected users that new user has joined
-        socket.broadcast.emit(chatConstants.JOINED, { userid, username });
+      });
 
-        // send message to all connected users that a user has left
-        socket.on('disconnect', (userid) => {
-          console.log('client disconnected from Chat Server..');
-          socket.broadcast.emit(chatConstants.LEFT, userid);
-        });
-        // send chat message to all connected users
-        socket.on(chatConstants.MSG, (msg) => {
-          socket.emit('chat', {
-            userid: userid,
-            msg: msg,
-            msgTime: new Date().getHours() + ':' + new Date().getMinutes(),
-          });
+      // send message to all connected users that new user has joined
+      socket.broadcast.emit(chatConstants.JOINED, { userid, username });
+
+      // send message to all connected users that a user has left
+      socket.on('disconnect', (userid) => {
+        console.log('client disconnected from Chat Server..');
+        socket.broadcast.emit(chatConstants.LEFT, userid);
+      });
+
+      // send chat message to all connected users
+      socket.on(chatConstants.MSG, (msg) => {
+        socket.emit('chat', {
+          userid: userid,
+          msg: msg,
+          msgTime: new Date().getHours() + ':' + new Date().getMinutes(),
         });
       });
     });

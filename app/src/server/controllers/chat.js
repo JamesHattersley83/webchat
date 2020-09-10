e4ff468238ee555d2beee272fd7e8efedece61c2
James Hattersley <jameshattersley83@gmail.com>
2020-09-10 20:12:27 +0100

    added try catch to controller

--- app/src/server/controllers/chat.js
+++ app/src/server/controllers/chat.js
@@ -3,16 +3,21 @@ const connect = require('../dbconnection');
 
 module.exports = {
   getfilteredChatHistory: (req, res, next) => {
-    let count = parseInt(req.params.count);
-    res.setHeader('content-type', 'application/json');
-    res.statusCode = 200;
-    connect.then(() => {
-      Chat.find({})
-        .limit(count)
-        .sort({ time: -1 })
-        .then((chat) => {
-          res.json(chat);
-        });
-    });
+    try {
+      let count = parseInt(req.params.count);
+      res.setHeader('content-type', 'application/json');
+      res.statusCode = 200;
+      connect.then(() => {
+        Chat.find({})
+          .limit(count)
+          .sort({ time: -1 })
+          .then((chat) => {
+            res.json(chat);
+          });
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   },
 };
